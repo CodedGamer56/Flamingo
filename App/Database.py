9763ac3e6f657bac4886ee0f4b8a90b0cfe0ead1
CodedGamer56 <codedgamer56@gmail.com>
2023-09-02 07:04:13 +0530

    Added: DB Methods, Log, and Render modules

--- App/Database.py
+++ App/Database.py
@@ -1,1 +1,38 @@
-# Database operations
+import mysql.connector as mysql
+
+from . import Log as cmd
+
+
+db_object = None
+cursor = None
+
+def init():
+    global db_object, cursor
+    try:
+        db_object = mysql.connect(host='localhost', user='root', database='Flamingo', password='mysql56')
+        cursor = db_object.cursor()
+        cmd.debug('Connection established with database')
+    except Exception as db_error:
+        cmd.error('Failed to connect to database - {error}'.format(error = db_error))
+
+def get_subjects(col = '*', ext = 'ORDER BY subject'):
+    query = 'SELECT {column} FROM journey {extension}'.format(column = col, extension = ext)
+    cursor.execute(query)
+    feedback = cursor.fetchall()
+    return feedback
+
+def create_journey(_subject):
+    query = 'INSERT INTO journey(subject) VALUE ("{subjectLabel}")'.format(subjectLabel = _subject)
+    cursor.execute(query)
+    feedback = cursor.fetchall()
+    return feedback
+
+def get_quests(col = '*', ext = ''):
+    query = 'SELECT {column} FROM quest {extension}'.format(column = col, extension = ext)
+    cursor.execute(query)
+    feedback = cursor.fetchall()
+    return feedback
+
+def end():
+    db_object.commit()
+    db_object.close()
---
+++ App/Render.py
@@ -0,0 +1,21 @@
+import colorful as cf
+
+from . import Database as db
+
+def render_menu():
+    # Topbar: Study, New, Edit, Remove, View, Settings
+    subjects = db.get_subjects()
+    
+    for i in subjects:
+        print(cf.bold_cyan(i[1]) + ':')
+        data = db.get_quests('name', 'WHERE parent_id = {id}'.format(id = i[0]))
+
+        for j in data:
+            print('\t', cf.grey(j[0]))
+            
+'''
+Design Ideas
+    - Spotify (Artist - Journey, Album - Quest, Song - Card)
+    - Github Commit Stats
+    - Recently Viewed
+'''
